0bba7bdb4dd52fe7aacf8a85a62200cb7612fd62
David <dwittyy@gmail.com>
2021-12-03 09:32:54 +0000

    2021 Day 3

---
+++ 2021/Day 3/a.py
@@ -0,0 +1,27 @@
+import sys, os
+
+sys.path.append(os.getcwd())
+from util.read import read, read_nums
+
+diagnostic_report = read(2021, 3)
+
+
+def most_frequent_bit(bits):
+    if bits.count("1") > bits.count("0"):
+        return "1"
+    else:
+        return "0"
+
+
+def find_power_consumption(nums):
+    l = len(nums[0])
+    gamma = ""
+    epsilon = ""
+    for i in range(0, l):
+        frequent = most_frequent_bit([num[i] for num in nums])
+        gamma += frequent
+        epsilon += "1" if frequent == "0" else "0"
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+print(find_power_consumption(diagnostic_report))
---
+++ 2021/Day 3/b.py
@@ -0,0 +1,31 @@
+import sys, os
+
+sys.path.append(os.getcwd())
+from util.read import read
+
+diagnostic_report = read(2021, 3)
+
+
+def frequent_bit(bits, extreme):
+    if extreme == "most":
+        result = "1" if bits.count("1") >= bits.count("0") else "0"
+    else:
+        result = "0" if bits.count("1") >= bits.count("0") else "1"
+    return result
+
+
+def find_rating(nums, extreme):
+    options = nums.copy()
+    for i in range(0, len(nums[0])):
+        frequent = frequent_bit([option[i] for option in options], extreme)
+        options = [bits for bits in options if bits[i] == frequent]
+        if len(options) == 1:
+            break
+    return int(options[0], 2)
+
+
+life_support_rating = find_rating(diagnostic_report, "most") * find_rating(
+    diagnostic_report, "least"
+)
+
+print(life_support_rating)
