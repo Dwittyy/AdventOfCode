1ad9563de9434268479574fd8bcf436fe6111c99
David <dwittyy@gmail.com>
2022-12-01 08:57:50 +0000

    2022 Day 1

---
+++ 2022/Day 01/a.py
@@ -0,0 +1,22 @@
+import sys, os
+sys.path.append(os.getcwd())
+from util.read import *
+from util.run import run
+
+@run
+def solve():
+    elf_calories = []
+    elf_calorie = []
+    for calorie in read_lines():
+        if calorie != "":
+            elf_calorie.append(int(calorie))
+        else:
+            elf_calories.append(elf_calorie)
+            elf_calorie = []
+    elf_calories.append(elf_calorie)
+
+    totals = [sum(holding) for holding in elf_calories]
+
+    return max(totals)
+
+solve()
