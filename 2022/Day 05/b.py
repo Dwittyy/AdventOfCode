fd26c5e0ac861004be52f0506120638f5ca85c2a
David <dwittyy@gmail.com>
2022-12-05 10:57:16 +0000

    2022 Day 5

---
+++ 2022/Day 05/b.py
@@ -0,0 +1,28 @@
+import sys, os
+sys.path.append(os.getcwd())
+from util.read import *
+from util.run import run
+import re
+
+def move_crates(stacks,amount,target,destination):
+    for i in range(amount):
+        stacks[destination].append(stacks[target].pop(-amount+i))
+
+@run
+def solve():
+    puzzle_input = read_lines()
+    splitter = puzzle_input.index('')
+
+    stacks = dict()
+    for idx, num in enumerate(puzzle_input[:splitter][-1]):
+        if num.isdigit():
+            stacks[int(num)] = [x[idx] for x in puzzle_input[:splitter-1] if x[idx].isalpha()][::-1]
+    
+    moves = [[int(x) for x in re.findall("[0-9]+",move)] for move in puzzle_input[splitter+1:]]
+
+    for move in moves:
+        move_crates(stacks,*move)
+
+    return "".join([x[-1] for x in stacks.values()])
+
+solve()
