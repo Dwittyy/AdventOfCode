c042ea69ddb9d3f581589c67eb2c53426e94f2e7
David <dwittyy@gmail.com>
2022-12-05 12:13:38 +0000

    Added read sections util function

--- util/read.py
+++ util/read.py
@@ -1,8 +1,13 @@
 import os, __main__
 
+def read_file(test=False):
+    return open(os.path.join(os.path.dirname(__main__.__file__),"test.txt" if test else "input.txt")).read()
+
 def read_lines(test=False):
-    filename = os.path.join(os.path.dirname(__main__.__file__),"test.txt" if test else "input.txt")
-    return open(filename).read().splitlines()
+    return read_file(test).splitlines()
 
 def read_nums(test=False):
     return [int(x) for x in read_lines(test)]
+
+def read_sections(test=False):
+    return [x.splitlines() for x in read_file(test).split("\n\n")]
