68a4f18eaf27bb0e34277c399b1c8f2dd31883f4
David <dwittyy@gmail.com>
2024-12-01 11:18:24 +0000

    2024 Day 1

--- 2024/Day 00/b.py
+++ 2024/Day 00/b.py
@@ -8,7 +8,7 @@ def solve():
     left = [int(line.split()[0]) for line in read_lines(test=False)]
     right = [int(line.split()[1]) for line in read_lines(test=False)]
 
-    distances = [abs(x-y) for (x,y) in zip(sorted(left),sorted(right))]
-    return sum(distances)
+    scores = [num * right.count(num) for num in left]
+    return sum(scores)
 
 solve()
